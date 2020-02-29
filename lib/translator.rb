873f2c089b70126c600d5c546010fd505662a1eb
Di Zheng <dizhengcindy@gmail.com>
2020-02-29 05:56:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,23 @@
 # require modules here
-
-def load_library
-  # code goes here
+require 'yaml'
+def load_library (file_path)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  data = YAML.load_file(file_path)
+  #has two keys, 'get_meaning' and 'get_emoticon'
+  # the keys inside the 'get_meaning' hash are the Japanese emoticons
+  #the emoticon keys inside the 'get_meaning' hash point to their meanings
+  
+  
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(we)
   # code goes here
+  #translate it to its Japanese version
 end
 
-def get_english_meaning
+def get_english_meaning(je)
   # code goes here
+  
+  #takes a Japanese emoticon and returns its meaning in English.
+  
 end
