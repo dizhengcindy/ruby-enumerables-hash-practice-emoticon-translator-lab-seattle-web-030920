e6ed29346a2263f474aa32d998d1c57ab44a73c8
Di Zheng <dizhengcindy@gmail.com>
2020-02-29 06:13:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'yaml'
 def load_library (file_path)
   data = YAML.load_file(file_path)
   result = {"get_meaning" => {}, "get_emoticon" => {}}
-  data.reduce do |memo, (key,array)|
+  data.reduce({}) do |memo, (key,array)|
       result["get_meaning"][array[1]] = key.to_s
       result["get_emoticon"][array[0]] = array[1]
       memo
