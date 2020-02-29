bd525b9739fc5b7a69a7a53780747c98a03f36c7
Di Zheng <dizhengcindy@gmail.com>
2020-02-29 06:18:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,6 +16,7 @@ def get_japanese_emoticon(file_path,we)
   #translate it to its Japanese version
   data = load_library (file_path)
   data["get_emoticon"][we]
+  
 end
 
 def get_english_meaning(file_path,je)
